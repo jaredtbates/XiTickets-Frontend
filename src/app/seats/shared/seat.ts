0b28f6d5eb6f9fbd5a2a2efd615e093a106f2c89
Jared Bates <computerwizjared@hotmail.com>
2016-09-23 17:43:19 -0500

    Seating chart layout done

--- src/app/seats/shared/seat.ts
+++ src/app/seats/shared/seat.ts
@@ -1,3 +1,3 @@
 export class Seat {
-  constructor(public id: number, public row: string, public column: number, public label: string, public blanksToLeft: number, public blanksToRight: number) { }
+  constructor(public id: string, public row: string, public column: number, public blanksToLeft: number, public blanksToRight: number) { }
 }
