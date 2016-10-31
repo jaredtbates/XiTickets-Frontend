347d90eb65480227b46493c0a2cb2040edb7965a
Jared Bates <computerwizjared@hotmail.com>
2016-10-30 20:36:44 -0500

    let's get checkout working soon

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SessionStorage } from 'ng2-webstorage';
+
+import { Seat, SeatService, Row, AvailabilityService } from '../seats/shared';
+import { ShowService, Show, Event } from '../shows/shared';
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  hovering: Seat = null;
+  @SessionStorage() selectedShow: Show;
+  @SessionStorage() selectedEvent: Event;
+  @SessionStorage() adultTickets: number;
+  @SessionStorage() childTickets: number;
+  @SessionStorage('selectedSeats') selectedSeatsString: string;
+  selectedSeats;
+  childCost: number = 3;
+  adultCost: number = 5;
 
   ngOnInit() {
+    this.selectedSeats = JSON.parse(this.selectedSeatsString);
   }
-
 }
